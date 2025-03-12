6ddb2d9cccafdc72f751a9d7834045cabdbd497a
Azim Sofi <azimmiskandar@gmail.com>
2025-03-12 15:25:10 +0900

    エラー修正

--- background.js
+++ background.js
@@ -6,6 +6,26 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "kaishaku-renshu") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const tab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+          return window.getSelection().toString();
+        }
+      }, function(results) {
+        if (results && results.length > 0) {
+          const selectedText = results[0].result;
+          chrome.action.openPopup();
+          chrome.storage.sync.set({ selectedText: selectedText });
+        }
+      });
+    });
+  }
+});
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "openPopup") {
